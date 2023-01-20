d03baf5a63090abfe6ae204ce2fd09130c414504
christian <cpchristiandelmonte@gmail.com>
2023-01-21 01:31:38 +0800

    add and configure tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-    />
+  <div id="app" class="flex flex-col justify-center items-center bg-slate-700 h-screen">
     <HelloWorld msg="Hello Vue 2 + Vite" />
   </div>
 </template>
@@ -18,12 +14,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
