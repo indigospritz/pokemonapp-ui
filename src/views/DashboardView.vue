cf9f17dabad189f2801401acb1533b05c20e276d
christian <cpchristiandelmonte@gmail.com>
2023-01-22 02:19:56 +0800

    created login and dashboard

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="grid bg-slate-700 text-center">
+    <h2>Dashboard</h2>
+    <ul>
+      <li>{{ list.name }}</li>
+    </ul>
+    {{ list.name }}
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+      alt=""
+    />
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+
+  mounted() {
+    this.fetchPokemon();
+  },
+
+  methods: {
+    async fetchPokemon() {
+      const url = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20"
+      );
+      const res = url.data.results;
+      this.list = res;
+    },
+  },
+};
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex h-auto flex-col items-center rounded bg-slate-300 p-4">
+    <div class="h-auto pb-8">
+      <h2 class="text-xl">Login form</h2>
+    </div>
+    <div class="grid gap-y-3">
+      <input type="text" placeholder="Username" class="rounded p-1" />
+      <input type="password" placeholder="Password" class="rounded p-1" />
+    </div>
+    <div class="w-full pt-8">
+      <button
+        @click="login"
+        class="flex w-full justify-center rounded bg-slate-600 py-2 text-slate-100"
+      >
+        Login
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  methods: {
+    login() {
+      this.$router.push("/dashboard");
+    },
+  },
+};
+</script>
